8a26df204fe9b396be1791fd26d11f4e19308baf
rajanraj2 <rajanraj253545@gmail.com>
2023-10-22 22:41:29 +0530

    Day 20 file add

---
+++ Day20_index.js
@@ -0,0 +1,19 @@
+// Link to problem :
+// https://codedamn.com/problem/71rsjBptmAxHVewrORrM0?challengeList=100-days-of-frontend
+
+// Challenge #1: Create two variables "num1" and "num2" and assign valid numbers to them
+let num1 = 5;
+let num2 = 10;
+
+// Challenge #2: Create a variable sum and assign the sum of num1 and num2 to the sum variable
+let sum = num1 + num2;
+
+// Challenge #3: Create a variable "product" and assign the product of num1 and num2 to the product variable
+let product = num1 * num2;
+
+// Challenge #4: Create two variables "flag1" and "flag2" and assign valid boolean values to them
+let flag1 = true;
+let flag2 = false;
+
+// Challenge #5: Create a variable "combinedFlag" which holds the logical AND value of "flag1" and "flag2" variables
+let combinedFlag = flag1 && flag2;
